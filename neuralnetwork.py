962ff310d0dbdd497c144684a5eeffbd87fe651c
Neo <nevanneo@gmail.com>
2024-09-29 16:39:07 +0700

    finished feedforward

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -2,42 +2,68 @@ import numpy as np
 from activationfunctions import *
 from initdata import *
 
+TRAININGSIZE = 0
+TESTINGSIZE = 0
 
 class Node:
     # init with random weights and bias
-    # inputCount is n of weights connected from prev
-    def __init__(self, inputCount):
-        self.weights = np.random.rand(inputCount)
+    # inputSize is n of weights connected from previous layer
+    def __init__(self, inputSize):
+        self.weights = np.random.rand(inputSize)
         self.bias = np.random.rand()
+        self.activation = 0
 
     # calculates dot product and applies activation function
-    def calcActivation(self, inputs, activationFunction):
+    def feedForward(self, inputs, activationFunction):
         z = np.dot(self.weights, inputs)
         self.activation = activationFunction.forward(z)
 
+# input layer will NOT be a layer
 class Layer:
-    # for the first layer, inputs will be training data
-    # for the rest, inputs will be activations from prev layers
-    def __init__(self, layerSize, inputs, activationFunction):        
+    def __init__(self, layerSize, inputSize, activationFunction):        
+        self.layerSize = layerSize
+        self.inputSize = inputSize            # number of weights
+        self.nodes = np.empty(self.layerSize) # empty array of nodes, stores activations
 
-        self.size = layerSize
-        self.inputCount = len(inputs)
+        # init nodes with random weights
+        for i in range(self.layerSize):
+            self.nodes[i] = Node(self.inputSize)
 
-        # init nodes
-        # remove
-        self.nodes = np.empty(self.size)
-        for i in range(self.size):
-            self.nodes[i] = Node(self.inputCount).calcActivation(inputs, activationFunction) 
-                                                    # remove
+    # inputs = activations from previous layer
+    # returns the activations
+    def feedForward(self, inputs):
+        for i in range(self.layerSize):
+            self.nodes[i] = self.nodes[i].feedForward[inputs]
+
+        return self.nodes
+                                  
 
 class NeuralNetwork:
-    def __init__(layerCount):
-        self.layers = np.empty(layerCount)
+    def __init__(self, layerCount):
+        self.layerCount = layerCount
+        self.layers = np.empty(layerCount)    # empty array of layers
+        self.outputs = np.empty(TRAININGSIZE) # array for storing outputs
 
-    def addLayer(self, layerIndex, layerSize, activationFunction):
-        self.layers[layerIndex] = Layer(layerSize, activationFunction)
+    # inputSize will just be the previous layer's layerSize (remember when writing main function)
+    def addLayer(self, layerIndex, layerSize, inputSize, activationFunction):
+        self.layers[layerIndex] = Layer(layerSize, inputSize, activationFunction)
+
+    # data = entire training set
+    # feeds data[n] to first layer, then feeds prev layer activations
+    # store outputs to self.outputs
+    # repeat for all training set
+    def feedForward(self, data):
+        for i in range(data):
+            activations = self.layers[0].feedForward(data[i])
+            for j in range(1, self.layerCount):
+                activations = self.layers[j].feedForward(activations)
+
+            outputs[i] = activations
+
+
+            
 
 
 
-x = Layer(10, [1, 2, 3], ReLu)
-print(x.nodes)
+if __name__ == "__main__":
+    nn = NeuralNetwork(4)
