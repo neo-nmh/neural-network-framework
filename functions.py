e07cd21235c48d0c28affd03f64e382611ec19fe
Neo <nevanneo@gmail.com>
2024-10-25 11:46:05 +0700

    what debugging looks like

--- functions.py
+++ functions.py
@@ -2,32 +2,85 @@ import numpy as np
 import math
 from neuralnetwork import CLASSCOUNT
 
+# activation functions operate on whole layer at a time
+
 class ReLu:
     def forward(activations):
-        return (max(0, activations)).item()
+        outputs = np.empty(len(activations), dtype=np.float32)
+        for i in range(len(activations)):
+            outputs[i] = max(0, activations[i])
 
-    def backward(weightedSum):
-        if weightedSum > 0:
-            return 1
-        else:
-            return 0
+        return outputs
+
+    # ReLu doesn't use 'activations' parameter
+    def backward(weightedSum, activations):
+        outputs = np.empty(len(weightedSum), dtype=np.float32)
+        for i in range(len(weightedSum)):
+            if weightedSum[i] > 0:
+                outputs[i] = 1
+            else:
+                outputs[i] = 0
+
+        return outputs
 
 
 class Sigmoid:
     def forward(activations):
-        return 1 / (1 + math.exp(-1 * activations))
+        outputs = np.empty(len(activations), dtype=np.float32)
+        for i in range(len(activations)):
+            outputs[i] = 1 / (1 + math.exp(-1 * activations[i]))
 
-    def backward(weightedSum):
-        sigmoid = 1 / (1 + math.exp(-1 * weightedSum))
-        return sigmoid * (1 - sigmoid)
+        return outputs
 
+    def backward(weightedSum, activations):
+        outputs = np.empty(len(weightedSum), dtype=np.float32)
+        for i in range(len(weightedSum)):
+            outputs[i] = activations[i] * (1 - activations[i])
+
+        return outputs
 
 class Softmax:
     def forward(activations):
-        return
+        outputs = np.empty((len(activations)), dtype=np.float32)
+        # denominator is sum of exp(activations)
+        expSum = 0
+        for i in activations:
+            expSum += math.exp(i) 
 
-    def backward(weightedSum):
-        return
+        # divide each exp(activation) by sum
+        for i in range(len(activations)):
+            outputs[i] = math.exp(activations[i]) / expSum
+
+        return outputs
+
+    # takes in a vector of weighted sums (z) with n rows
+    # derivative w.r.t z[i] is a vector of n rows,
+    # where nth row is how activation[n] changes w.r.t z[i]
+    # these vectors can be represented as a jacobian matrix (n x n)
+    def backward(weightedSum, activations):
+        # temporary array for each column of jacobian 
+        print("")
+        print("inside softmax backward")
+        print("weightedsum", weightedSum)
+        print("activations", activations)
+        jacobian = np.empty(len(weightedSum), dtype=np.float32)
+
+        # sum of each column of jacobian
+        outputs = np.empty(len(weightedSum), dtype=np.float32)
+
+        # i = inputs, j = outputs
+        # when i == j, derivative is different
+        for i in range(len(weightedSum)):
+            for j in range(len(weightedSum)):
+                if i == j:
+                    jacobian[j] = activations[i] * (1 - activations[i])
+                else:
+                    jacobian[j] = (-1 * activations[j]) * activations[i]
+            outputs[i] = np.sum(jacobian)
+        print("outputs ", outputs)
+
+        print("")
+        return outputs # returns sum of each column of jacobian matrix
 
 
 class Loss:
--- neuralnetwork.py
+++ neuralnetwork.py
@@ -3,35 +3,30 @@ from functions import *
 from initdata import *
 from visualizations import *
 
-TRAININGSIZE = 2
-TESTINGSIZE = 0
-CLASSCOUNT = 5
-LEARNINGRATE = 0.000000001
 
 class Node:
-    # init with random params, weights are weighted connected from prev layers
+    # init with random or np.ones params, weights are connected from prev layers
     def __init__(self, inputSize):
-        self.inputSize = inputSize
-        self.inputs = np.empty(inputSize, dtype=np.float32)
-        self.weights = np.float32(np.ones(inputSize))
-        self.bias = np.float32(np.ones(1))
+        self.inputSize   = inputSize
+        self.inputs      = np.empty(inputSize, dtype=np.float32)
+        self.weights     = np.float32(np.random.rand(inputSize))
+        self.bias        = np.float32(np.random.rand(1))
+        self.weightedSum = np.float32(0)
 
-    # calculate and stores activation and weighted sum
-    def calculateActivation(self, inputs, activationFunction):
+    # calculate and stores weighted sum (not activation because of softmax)
+    def calculateWeightedSum(self, inputs):
         self.inputs = inputs
-        z = np.dot(self.weights, self.inputs) + self.bias
-        self.weightedSum = z 
-        self.activation = activationFunction.forward(z)
+        self.weightedSum = np.dot(self.weights, self.inputs) + self.bias
+        return self.weightedSum
 
     # adjust weights and bias
     def backPropogate(self, gradient):
+        print("before: ", self.weights[1])
         for i in range(self.inputSize):
             self.weights[i] -= LEARNINGRATE * (gradient * self.inputs[i]) # dL/dw
-            """
-            print("gradient")
-            print(LEARNINGRATE * (gradient * self.inputs[i]))
-            print("")
-            """
+
+        print("adjusted by: ", LEARNINGRATE * (gradient * self.inputs[i]))
+        print("after: ", self.weights[1])
 
         self.bias -= LEARNINGRATE * gradient                              # dL/db 
 
@@ -41,31 +36,38 @@ class Node:
 class Layer:
     def __init__(self, layerSize, inputSize, activationFunction):        
         self.layerSize = layerSize
-        self.inputSize = inputSize                        # number of weights connected to each node
+        self.inputSize = inputSize                     # number of weights connected to each node
         self.activationFunction = activationFunction
-        self.nodes = np.empty(self.layerSize, dtype=Node) # layers store array of nodes
-        for i in range(self.layerSize):                   # init random weights for each node
-            self.nodes[i] = Node(self.inputSize)
+        self.nodes = np.empty(layerSize, dtype=Node)   # layer stores array of nodes
+        for i in range(layerSize):                     # init random weights for each node
+            self.nodes[i] = Node(inputSize)
+        self.activations = np.empty(layerSize, dtype=np.float32)
+        self.weightedSum = np.empty(layerSize, dtype=np.float32)
 
     # inputs = activations from previous layer
     def calculateActivation(self, inputs):
-        activations = np.empty(self.layerSize, dtype=np.float32)
+        # calculates weighted sum for each node
         for i in range(self.layerSize):
-            self.nodes[i].calculateActivation(inputs, self.activationFunction)
-            activations[i] = self.nodes[i].activation
+            self.weightedSum[i] = (self.nodes[i].calculateWeightedSum(inputs)).item() # .item ensures it is a scalar
 
-        return activations # returns array of activations
+        # pass whole layer through activation function
+        self.activations = self.activationFunction.forward(self.weightedSum)
 
-    # for each node, adjust weights and bias
-    # gradient = dL/da of each node 
+        return self.activations # returns vector of activations
+
+    # calculates dLdz, adjusts w and b for each node in layer
+    # gradient = vector of dL/da of each node 
     def backPropogate(self, gradient):
-        dLdz = 0
-        for i in range(self.layerSize):
-            temp = gradient[i] * self.activationFunction.backward(self.nodes[i].weightedSum) # gradient * da/dz of each node
-            self.nodes[i].backPropogate(temp)
-            dLdz += temp
+        print("")
+        print("first grad: ", gradient)
+        dLdz = gradient * self.activationFunction.backward(self.weightedSum, self.activations) # gradient * da/dz (vector)
+        print("gradient before node: ", dLdz)
 
-        return dLdz # return sum of dL/dz for this layer
+        # adjust w and b for each node
+        for i in range(self.layerSize):   
+            self.nodes[i].backPropogate(dLdz[i])
+
+        return np.sum(dLdz) # return sum of dL/dz for this layer (used for backprop to prev layers)
 
     
 class NeuralNetwork:
@@ -85,9 +87,16 @@ class NeuralNetwork:
     # using 1 training example at a time
     def feedForward(self, label, dataIndex):
         activations = self.layers[0].calculateActivation(self.trainingData[dataIndex])
+        print("FIRST LAYER")
+        print(self.layers[0].activationFunction)
+        print(activations)
+        print("")
 
         for i in range(1, self.layerCount): # starting from 2nd layer
+            print("SECOND LAYER")
             activations = self.layers[i].calculateActivation(activations)
+            print(self.layers[i].activationFunction)
+            print(activations)
 
         # storing activations 
         self.outputs[dataIndex] = activations
@@ -95,42 +104,56 @@ class NeuralNetwork:
         # calculate and store loss
         self.loss[dataIndex] = Loss.forward(activations, label)   
 
-    # passes array of gradients to layer recursively
+    # passes vector of gradients to each layer recursively
     def backPropogate(self, label, dataIndex): 
-        dLda = Loss.backward(self.outputs[dataIndex], label)
+        print("")
+        print("START BACKPROP")
+        print("--------------")
+        dLda = Loss.backward(self.outputs[dataIndex], label) # how loss changes w.r.t final layer output
 
-        # finds vector of dLda for every layer, sends to layer to calculate gradients for weights
-        for i in range(self.layerCount - 1, 0, -1):
-            temp = self.layers[i].backPropogate(dLda)                           # backpropogates dL/da, returns sum of dL/dz
-            dLdz = np.full(len(self.layers[i-1].nodes), temp, dtype=np.float32) # array of sum of dLdz (all values same)
+        # finds vector of dLda for every layer, sends this vector to layer class to calculate gradients of weights and bias for this layer
+        for i in range(self.layerCount - 1, 0, -1):                             # starting from 2nd last layer,
+            temp = self.layers[i].backPropogate(dLda)                           # sends dL/da to layer i, returns sum of dL/dz of layer i
+            exit()
 
-            dzda = np.zeros(len(self.layers[i - 1].nodes), dtype=np.float32)    # how weighted sums changes wrt each activation in layer[i-1]
-            for j in range(len(self.layers[i - 1].nodes)):                      # for each node in layer[i-1], sum the weights of layer ahead
-                for k in range(len(self.layers[i].nodes)):
-                    dzda[j] += self.layers[i].nodes[k].weights[j]
+            # note: weights + bias of layer have been adjusted by this point, the rest is calculating derivatives used for prev layers
+
+            dLdz = np.full(len(self.layers[i - 1].nodes), temp, dtype=np.float32) # array of sum of dLdz (all values same)
+
+            dzda = np.zeros(len(self.layers[i - 1].nodes), dtype=np.float32)    # how weighted sums changes w.r.t each activation in layer[i-1]
+            for j in range(len(self.layers[i - 1].nodes)):                      
+                for k in range(len(self.layers[i].nodes)):                      # for each node in layer[i-1], sum the weights for each 
+                    dzda[j] += self.layers[i].nodes[k].weights[j]               # node in layer ahead that connects to this node
 
             dLda = dLdz * dzda
 
 
+TRAININGSIZE = 4
+TESTINGSIZE = 0
+CLASSCOUNT = 2      # size of label vector
+LEARNINGRATE = 0.1
+EPOCHS = 2
+
+# testing
 if __name__ == "__main__":
-    # XOR test
     trainingData = np.array([
-        [1, 1],
-        [2, 2]
+        [0, 0],
+        [1, 0],
+        [0, 1],
+        [1, 1]
     ])
 
     labels = np.array([
-        [1000, 1000, 1000, 1000, 1000],
-        [2000, 2000, 2000, 2000, 2000]
+        [0,1],
+        [1,0],
+        [1,0],
+        [0,1]
     ])
 
-    nn = NeuralNetwork(layerCount=4, trainingData=trainingData)
-    nn.addLayer(layerIndex=0, inputSize=2, layerSize=6, activationFunction=ReLu)
-    nn.addLayer(layerIndex=1, inputSize=6, layerSize=10, activationFunction=ReLu)
-    nn.addLayer(layerIndex=2, inputSize=10, layerSize=7, activationFunction=ReLu)
-    nn.addLayer(layerIndex=3, inputSize=7, layerSize=5, activationFunction=ReLu)
+    nn = NeuralNetwork(layerCount=2, trainingData=trainingData)
+    nn.addLayer(layerIndex=0, inputSize=2, layerSize=2, activationFunction=Sigmoid)
+    nn.addLayer(layerIndex=1, inputSize=2, layerSize=CLASSCOUNT, activationFunction=Softmax)
 
-    EPOCHS = 1000        
 
     losses = np.empty((EPOCHS, TRAININGSIZE), dtype=np.float32)
     for i in range(EPOCHS):
@@ -141,10 +164,13 @@ if __name__ == "__main__":
         # 2d array of losses for each epoch
         losses[i] = nn.loss
 
-        print("outputs, loss")
+        print("outputs")
         print(nn.outputs)
+        print("")
+        print("loss")
         print(nn.loss)
         print("")
-
-    plotLoss(EPOCHS, losses)
+"""
+plotLoss(EPOCHS, losses)
+"""
         
