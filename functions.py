18e4acb3a97b4be533908858caf6c148b63f159b
Neo <nevanneo@gmail.com>
2025-07-08 10:10:22 +0700

    can converge using 1 training example

--- functions.py
+++ functions.py
@@ -1,8 +1,10 @@
 import numpy as np
+import math
+from neuralnetwork import CLASSCOUNT
 
 class ReLu:
-    def forward(input):
-        return np.maximum(0, input)
+    def forward(activations):
+        return np.maximum(0, activations)
 
     def backward(weightedSum):
         if weightedSum > 0:
@@ -11,12 +13,37 @@ class ReLu:
             return 0
 
 
-# 1/2 MSE loss function
-def calculateLoss(activations, label):
-    loss = 0
-    for i in range(len(activations)):
-        loss += (np.square(label[i] - activations[i]))
-        
-    loss /= (2 * len(activations))
+class Sigmoid:
+    def forward(activations):
+        return np.float32(1 / (1 + math.exp(-1 * activations)))
 
-    return loss
+    def backward(weightedSum):
+        sigmoid = 1 / (1 + math.exp(-1 * weightedSum))
+        return np.float32(sigmoid * (1 - sigmoid))
+
+
+class Softmax:
+    def forward(activations):
+        return
+
+    def backward(weightedSum):
+        return
+
+
+class Loss:
+    # 1/2 MSE loss function
+    def forward(activations, label):
+        loss = 0
+        label = np.array([label])
+        for i in range(len(activations)):
+            loss += (np.square(label[i] - activations[i]))
+        loss /= (2 * len(activations))
+        return loss
+
+    # dL/da = -1/n(y-a)
+    def backward(activations, label):
+        label = np.array([label])
+        grad = np.empty(CLASSCOUNT, dtype=np.float32)
+        for i in range(CLASSCOUNT):
+            grad[i] = (-1 / CLASSCOUNT) * (label[i] - activations[i]) 
+        return grad
